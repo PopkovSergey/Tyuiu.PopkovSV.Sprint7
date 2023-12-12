8490601b62c16ba8afd88170ba40c22f14f40607
Tyuiu.PopkovSV <popkovsergej10@gmail.com>
2023-12-12 21:11:21 +0300

    Add project files.

---
+++ Tyuiu.PopkovSV.Sprint7.Project.V13.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.PopkovSV.Sprint7.Project.V13.Lib
+{
+    public class DataService
+    {
+    }
+}
---
+++ Tyuiu.PopkovSV.Sprint7.Project.V13.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tyuiu.PopkovSV.Sprint7.Project.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
